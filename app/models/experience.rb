c9daa82d18d54b34c4f3a6ce32f258fe1c8dcfa4
swift-x-github <swift.x@yahoo.com>
2020-11-18 15:18:59 +0200

    ip field added

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -7,20 +7,12 @@ class Experience < ApplicationRecord
   validates :name, presence:true
  
 
-  def total_raiting_summa (ik)
+  def total_raiting (ik)
     res = (rates.where(experience_id: ik).pluck( :rate).sum.to_f / rates.where(experience_id: ik).pluck( :rate).count.to_f) 
     res > 0 ? res : 0    
   end
   
   
-  #def total_raiting_summa (ik)
-  #  rates.select( :rate).where(experience_id: ik).to_s.sum
-  #end
-  # формула рейтинга
-  def total_rating
-    #@total_rating ||= (total_raiting_summa.to_i / total_rates.to_i)
-    @total_rating ||= (10.to_f / total_rates_number.to_f)
-  end
 
 
   # это сколько всего записей в этом експиренсе
